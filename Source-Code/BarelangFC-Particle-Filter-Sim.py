993ed5d8106c2e06699e6fd4eae3b761dca82f54
Eko Rudiawan Jamzuri <ekorudiawan@gmail.com>
2018-05-25 13:42:39 +0700

    Update omni motion model

--- Source-Code/BarelangFC-Particle-Filter-Sim.py
+++ Source-Code/BarelangFC-Particle-Filter-Sim.py
@@ -15,6 +15,7 @@ import cv2
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
 
+# Satuan dalam CM
 fieldLength = 900
 fieldWidth = 600
 totalParticles = 100
@@ -22,14 +23,16 @@ totalLandmarks = 2
 
 # Global Variable
 # Robot position 1D array
-robotPosition = np.zeros((3), dtype=int)
+robotGlobalPosition = np.zeros((3), dtype=int)
+robotLocalPosition = np.zeros((3), dtype=int)
 
 # Estimate position
 estimatePosition = np.zeros((3), dtype=int)
 
 # Landmarks position 2D array
 landmarksPosition = np.zeros((totalLandmarks, 2), dtype=int)
-particlesPosition = np.zeros((totalParticles, 3), dtype=int)
+particlesGlobalPosition = np.zeros((totalParticles, 3), dtype=int)
+particlesLocalPosition = np.zeros((totalParticles, 3), dtype=int)
 distanceRobotToLandmarks = np.zeros((totalLandmarks), dtype=int)
 distanceParticlesToLandmarks = np.zeros((totalParticles, totalLandmarks), dtype=int)
 particlesWeight= np.zeros((totalParticles), dtype=float)
@@ -93,11 +96,16 @@ def convertVel(robotId, inputVel):
 
 def main():
     # Initialize robot position
-    robotPosition[0] = uniform(0, fieldLength)
-    robotPosition[1] = uniform(0, fieldWidth)
+    robotGlobalPosition[0] = uniform(0, fieldLength)
+    robotGlobalPosition[1] = uniform(0, fieldWidth)
     # Set initial location of robot
-    robotPosition[0] = 50
-    robotPosition[1] = 300
+    robotGlobalPosition[0] = 0
+    robotGlobalPosition[1] = 0
+    robotGlobalPosition[2] = 90 # Heading
+
+    robotLocalPosition[0] = 0
+    robotLocalPosition[1] = 0
+    robotLocalPosition[2] = robotGlobalPosition[2] # Heading
 
     # Initialize landmark position
     landmarksPosition[:, 0] = uniform(0, fieldLength, size=totalLandmarks)
@@ -116,16 +124,16 @@ def main():
 
     # Create random position of particles
     # print 'Process ==> Create random particles'
-    particlesPosition[:, 0] = uniform(0, fieldLength, size=totalParticles)
-    particlesPosition[:, 1] = uniform(0, fieldWidth, size=totalParticles)
+    particlesGlobalPosition[:, 0] = uniform(0, fieldLength, size=totalParticles)
+    particlesGlobalPosition[:, 1] = uniform(0, fieldWidth, size=totalParticles)
     # print 'Particles position :'
-    # print particlesPosition
+    # print particlesGlobalPosition
 
     mapImage = np.zeros((800,1100,3), np.uint8)
     
     # Simualte velocity value
     # X Y alpha velocity command from robot
-    simVelocity = np.empty([3], dtype=int)
+    realVelocity = np.empty([3], dtype=int)
     # Loop every 1 second
     deltaTime = 1 
 
@@ -134,18 +142,18 @@ def main():
     # axes.set_xlim(0, 900)
     # axes.set_ylim(0, 600)
     # plt.title('Barelang FC - Particle Filter Simulation')
-    # particlesData, = axes.plot(particlesPosition[:,0], particlesPosition[:,1], 'ro')
+    # particlesData, = axes.plot(particlesGlobalPosition[:,0], particlesGlobalPosition[:,1], 'ro')
     # robotPosData, = axes.plot(robotPosition[0], robotPosition[1], 'ro', color='g')
     # estimatePosData, = axes.plot(estimatePosition[0], estimatePosition[1], 'ro', color='b')
     # landmarksPosData, = axes.plot(landmarksPosition[:,0], landmarksPosition[:,1], 'ro', color='y')
 
     # Create UDP client to receive data from kinematic
-    try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
-        sock.bind((UDP_IP, UDP_PORT))
-    except socket.error:
-        print 'Failed to create socket'
-        sys.exit()
+    # try:
+    #     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
+    #     sock.bind((UDP_IP, UDP_PORT))
+    # except socket.error:
+    #     print 'Failed to create socket'
+    #     sys.exit()
 
     loop = 0
 
@@ -164,66 +172,76 @@ def main():
         # cv2.imwrite("mapImage.jpg", mapImage)
         # break
 
-        # Get data from kinematic
-        data, _ = sock.recvfrom(1024) # buffer size is 1024 bytes
-        # print "Vel Input : ", data
+        # # Get data from kinematic
+        # data, _ = sock.recvfrom(1024) # buffer size is 1024 bytes
+        # # print "Vel Input : ", data
         velFromKinematic = np.zeros((3))
-        strVelFromKinematic = data.split(",")
-        velFromKinematic[0] = float(strVelFromKinematic[0])
-        velFromKinematic[1] = float(strVelFromKinematic[1])
-        velFromKinematic[2] = float(strVelFromKinematic[2])
+        # strVelFromKinematic = data.split(",")
+        # velFromKinematic[0] = float(strVelFromKinematic[0])
+        # velFromKinematic[1] = float(strVelFromKinematic[1])
+        # velFromKinematic[2] = float(strVelFromKinematic[2])
+
+        velFromKinematic[0] = 0.01
+        velFromKinematic[1] = 0.00
+        velFromKinematic[2] = 0.00
 
         # print "Input Vel : ", velFromKinematic
 
         # X = 5, Y = 0, alpha = 0
-        # simVelocity[0] = 10 
-        # simVelocity[1] = 0 
-        # simVelocity[2] = 0 
+        # realVelocity[0] = 10 
+        # realVelocity[1] = 0 
+        # realVelocity[2] = 0 
 
-        simVelocity = convertVel(robotID, velFromKinematic)
-        # print "Output Vel : ", simVelocity
-
+        # Nanti diganti nama jadi real velocity
+        realVelocity = convertVel(robotID, velFromKinematic)
+        # print "Real Velocity : ", realVelocity
         # Simulate robot movement
-        vectVelocity  = math.sqrt(pow(simVelocity[0], 2) + pow(simVelocity[1], 2))
+        # if robotLocalPosition[0] >= 0 and robotLocalPosition[0] < fieldLength and robotLocalPosition[1] >= 0 and robotLocalPosition[1] < fieldWidth:
+        robotLocalPosition[0] += realVelocity[0] * deltaTime
+        robotLocalPosition[1] += realVelocity[1] * deltaTime
+        robotLocalPosition[2] += realVelocity[2] * deltaTime
 
-        if simVelocity[2] != 0:
-            robotPosition[0] += (vectVelocity / simVelocity[2]) * ( math.sin(math.radians(robotPosition[2])) + (simVelocity[2] * deltaTime) ) - math.sin(math.radians(robotPosition[2]))
-            robotPosition[1] += (vectVelocity / simVelocity[2]) * ( math.cos(math.radians(robotPosition[2])) - math.cos(math.radians(robotPosition[2])) + (simVelocity[2] * deltaTime))
-            robotPosition[2] += simVelocity[2] * deltaTime
-            # // Add measurements to particles
-            # particles[i].x += (velocity/yaw_rate) * (sin(particles[i].theta + (yaw_rate * delta_t)) - sin(particles[i].theta));
-            # particles[i].y += (velocity/yaw_rate) * (cos(particles[i].theta) - cos(particles[i].theta + (yaw_rate * delta_t)));
-            # particles[i].theta += yaw_rate * delta_t;
-        else:
-            robotPosition[0] += simVelocity[0] * deltaTime 
-            robotPosition[1] += simVelocity[1] * deltaTime 
+        # Create matrix rotation
+        theta = np.radians(robotLocalPosition[2])
+        c, s = np.cos(theta), np.sin(theta)
+        R = np.array(((c,-s), (s, c)))
+        # Multiply rotation matrix with robot local position x dan y
+        npOutMatMul = np.matmul(R, robotLocalPosition[:2]) 
+        # print npOutMatMul
 
-        # } else {
-        # // Add measurements to particles
-        # particles[i].x += velocity * delta_t * cos(particles[i].theta);
-        # particles[i].y += velocity * delta_t * sin(particles[i].theta);
-        # // Theta will stay the same due to no yaw_rate
-        
-        # }
-        # print "Vel : ", vectVelocity
-        # robotPosition[0] += simVelocity[0]
-        # robotPosition[1] += simVelocity[1]
-        print 'Robot position : (%d, %d, %d)'%(robotPosition[0], robotPosition[1], robotPosition[2])
+        # if robotGlobalPosition[0] >= 0 and robotGlobalPosition[0] < fieldLength and robotGlobalPosition[1] >= 0 and robotGlobalPosition[1] < fieldWidth:
+        robotGlobalPosition[0] += npOutMatMul[0]
+        robotGlobalPosition[1] += npOutMatMul[1]
+        robotGlobalPosition[2] = robotLocalPosition[2]
+
+        # print 'Robot position : (%d, %d, %d)'%(robotGlobalPosition[0], robotGlobalPosition[1], robotGlobalPosition[2])
 
         # Predict movement of particles
         # print 'Process ==> Predict movement of particles'
-        particlesPosition[:,0] += int(simVelocity[0])
-        particlesPosition[:,1] += int(simVelocity[1])
+        particlesLocalPosition[:,0] += int(realVelocity[0])* deltaTime
+        particlesLocalPosition[:,1] += int(realVelocity[1]) * deltaTime
+        particlesLocalPosition[:,2] += int(realVelocity[2]) * deltaTime
         # Simulate noise movement of robot with error stddev = 10
-        particlesPosition[:,0] = normal(particlesPosition[:,0], 10)
-        particlesPosition[:,1] = normal(particlesPosition[:,1], 10)
+        particlesLocalPosition[:,0] = normal(particlesLocalPosition[:,0], 10)
+        particlesLocalPosition[:,1] = normal(particlesLocalPosition[:,1], 10)
+        particlesLocalPosition[:,2] = normal(particlesLocalPosition[:,1], 3)
+
+        for i in range (0,totalParticles):
+            # Create matrix rotation
+            theta = np.radians(particlesLocalPosition[i,2])
+            c, s = np.cos(theta), np.sin(theta)
+            R = np.array(((c,-s), (s, c)))
+            npOutMatMul = np.matmul(R, particlesLocalPosition[i,:2]) 
+            particlesGlobalPosition[i,0] += npOutMatMul[0]
+            particlesGlobalPosition[i,1] += npOutMatMul[1]
+            particlesGlobalPosition[i,2] = particlesLocalPosition[i,2]
 
         # print 'Particles position'
-        # print particlesPosition
+        # print particlesGlobalPosition
         # Update measurement
         # Measurement distance between robot and landmarks
         for i in range (0,totalLandmarks):
-            distanceRobotToLandmarks[i] = math.hypot(robotPosition[0] - landmarksPosition[i,0], robotPosition[1] - landmarksPosition[i,1])
+            distanceRobotToLandmarks[i] = math.hypot(robotGlobalPosition[0] - landmarksPosition[i,0], robotGlobalPosition[1] - landmarksPosition[i,1])
             # Simulate noise with random gaussian from measurment
             distanceRobotToLandmarks[i] = normal(distanceRobotToLandmarks[i], 10)
         # print 'Distance Robot to Landmarks :'
@@ -231,7 +249,7 @@ def main():
         # Measurement distance between particles and landmarks
         for i in range (0, totalParticles):
             for j in range (0, totalLandmarks):
-                distanceParticlesToLandmarks[i,j] = math.hypot(particlesPosition[i,0] - landmarksPosition[j,0], particlesPosition[i,1] - landmarksPosition[j,1])
+                distanceParticlesToLandmarks[i,j] = math.hypot(particlesGlobalPosition[i,0] - landmarksPosition[j,0], particlesGlobalPosition[i,1] - landmarksPosition[j,1])
         # print 'Distance Particles to Landmarks :'
         # print distanceParticlesToLandmarks
         # Calculating weight
@@ -251,26 +269,26 @@ def main():
         # print particlesWeight
 
         # Calculate estimate position
-        pos = particlesPosition[:, 0:2]
+        pos = particlesGlobalPosition[:, 0:2]
         mean = np.average(pos, weights=particlesWeight, axis=0)    
 
         estimatePosition = mean
         # print 'Estimate Position : (%d,%d)'%(estimatePosition[0],estimatePosition[1])
         
-        drawParticles = False
+        drawParticles = True
         if drawParticles == True:
             for i in range (0, totalParticles):
-                cv2.circle(mapImage,(particlesPosition[i,0]+100, 800 - (particlesPosition[i,1]+100) ), 7, (0,0,255), -1)
+                cv2.circle(mapImage,(particlesGlobalPosition[i,0]+100, 800 - (particlesGlobalPosition[i,1]+100) ), 7, (0,0,255), -1)
 
         
-        cv2.circle(mapImage,(robotPosition[0]+100, 800 - (robotPosition[1]+100) ), 7, (0,255,255), -1)
-        # cv2.circle(mapImage,(int(estimatePosition[0])+100, 800 - (int(estimatePosition[1])+100)), 7, (255,0,0), -1)
+        cv2.circle(mapImage,(robotGlobalPosition[0]+100, 800 - (robotGlobalPosition[1]+100) ), 7, (0,255,255), -1)
+        cv2.circle(mapImage,(int(estimatePosition[0])+100, 800 - (int(estimatePosition[1])+100)), 7, (255,0,0), -1)
 
-        textLine = "R{} Position (X, Y, Theta) : ({}, {}, {})".format(robotID, int(estimatePosition[0]), int(estimatePosition[1]), int(estimatePosition[1]))
+        textLine = "R{} Global Position (X, Y, Theta) : ({}, {}, {})".format(robotID, int(robotGlobalPosition[0]), int(robotGlobalPosition[1]), int(robotGlobalPosition[2]))
         cv2.putText(mapImage, textLine, (10,20), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 0, 127), 1, cv2.LINE_AA)
 
         # Enable GUI Streaming
-        showGUI = False
+        showGUI = True
         if showGUI:
             cv2.imshow("Barelang FC - Localization ", mapImage)
         # Enable URL Streaming
@@ -278,8 +296,8 @@ def main():
         if streamUrl == True:
             cv2.imwrite('stream.jpg', mapImage)
             yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + open('stream.jpg', 'rb').read() + b'\r\n')
-        # particlesData.set_xdata(particlesPosition[:,0])
-        # particlesData.set_ydata(particlesPosition[:,1])
+        # particlesData.set_xdata(particlesGlobalPosition[:,0])
+        # particlesData.set_ydata(particlesGlobalPosition[:,1])
         # robotPosData.set_xdata(robotPosition[0])
         # robotPosData.set_ydata(robotPosition[1])
         # estimatePosData.set_xdata(estimatePosition[0])
@@ -291,23 +309,23 @@ def main():
                 
         # Resample
         indexHighestWeight = np.argmax(particlesWeight)
-        xHighest = particlesPosition[indexHighestWeight,0]
-        yHighest = particlesPosition[indexHighestWeight,1]
+        xHighest = particlesGlobalPosition[indexHighestWeight,0]
+        yHighest = particlesGlobalPosition[indexHighestWeight,1]
 
         _10PercentParticle = int(totalParticles * 0.1)
 
         for i in range (0, _10PercentParticle):
-            particlesPosition[i,0] = uniform(0, fieldLength)
-            particlesPosition[i,1] = uniform(0, fieldWidth)
+            particlesGlobalPosition[i,0] = uniform(0, fieldLength)
+            particlesGlobalPosition[i,1] = uniform(0, fieldWidth)
 
         _90PercentParticle = totalParticles - _10PercentParticle
 
         for i in range (_10PercentParticle + 1, _90PercentParticle):
-            particlesPosition[i,0] = normal(xHighest, 20)
-            particlesPosition[i,1] = normal(yHighest, 20) 
+            particlesGlobalPosition[i,0] = normal(xHighest, 20)
+            particlesGlobalPosition[i,1] = normal(yHighest, 20) 
 
         loop += 1     
-        k = cv2.waitKey(1000)
+        k = cv2.waitKey(deltaTime * 1000)
         if k == ord('x'):         # X
             break
 
